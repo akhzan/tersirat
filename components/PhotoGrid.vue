1bcc010517f47ce2cfc900c67c351320e568d536
Syafrizal Akhzan <rizal.akhzan@gmail.com>
2019-01-02 09:44:55 +0700

    remove old code photogrid

--- components/PhotoGrid.vue
+++ components/PhotoGrid.vue
@@ -1,29 +1,4 @@
 <template>
-  <!-- <v-layout class="photo-grid">
-    <v-flex xs12 sm12 md12>
-      <div>
-        <v-container grid-list-sm fluid>
-          <v-layout row wrap>
-            <v-flex v-for="n in 9" :key="n">
-              <v-card flat tile class="d-flex">
-                <v-img
-                  :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-                  :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
-                  height="250"
-                  width="250"
-                  class="grey lighten-2"
-                >
-                  <v-layout slot="placeholder" fill-height align-center justify-center ma-0>
-                    <v-progress-circular indeterminate color="grey lighten-5"/>
-                  </v-layout>
-                </v-img>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </div>
-    </v-flex>
-  </v-layout> -->
   <div class="photo-grid__row">
     <div class="photo-grid__column">
       <v-img src="https://picsum.photos/500/300?image=15" style="width:100%" />
